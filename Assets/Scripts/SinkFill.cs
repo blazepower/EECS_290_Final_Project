90ee7322e20ce865e04da214b06a17da13e90991
Rishik Hombal <hombalrishik@gmail.com>
2020-04-06 20:36:15 -0400

    Wrote script to fill watering can from sink

---
+++ Assets/Scripts/SinkFill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkFill : MonoBehaviour{
+    private int maxCapacity = 50;
+    [SerializeField] private GameObject waterCan;
+    public Color highlightColor = Color.cyan;
+
+    public void OnMouseEnter(){
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null){
+            sprite.color = highlightColor;
+        }
+    }
+
+    public void OnMouseDrag(){
+        while (waterCan.capacity <= maxCapacity){
+            waterCan.capacity += 10 * Time.deltaTime;
+        }
+    }
+    
+}
