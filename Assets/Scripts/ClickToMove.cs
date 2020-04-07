f71956ebab7e824046a6cbb62f0cca3d769f639f
Matthew Garcia <mattgar62@gmail.com>
2020-04-07 16:27:36 -0400

    Added Sprites to functionality

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class ClickToMove : MonoBehaviour{
     public Vector3 mousePos;
 
+
     // Update is called once per frame
     void Update(){
         if (Input.GetKeyDown(KeyCode.Mouse0)){
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, mousePos, Time.deltaTime * 5);
+        transform.position = Vector3.MoveTowards(transform.position, mousePos, Time.deltaTime * 25);
     }
 }
