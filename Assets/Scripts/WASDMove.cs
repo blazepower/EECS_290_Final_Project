8486d7905e79218a12a065aaa7a6121be2fd5bb3
Anna <amd219@case.edu>
2020-04-19 19:51:37 -0400

    small WASDMove edit

--- Assets/Scripts/WASDMove.cs
+++ Assets/Scripts/WASDMove.cs
@@ -7,12 +7,27 @@ public class WASDMove : MonoBehaviour
     public static int moveSpeed = 9;
     public int moveSpeedDisplay = moveSpeed;
     public Rigidbody2D florist;
+    public Animator animator;
     Vector2 movement;
+    Vector2 facing;
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //Sets the values of the parameters in the animator so that animations match character movement
+        animator.SetFloat("horMove", movement.x);
+        animator.SetFloat("vertMove", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+
+        //Character position for when they are not moving
+        if(movement.x != 0 || movement.y != 0)
+        {
+            facing = movement;
+        }
+        animator.SetFloat("horFace", facing.x);
+        animator.SetFloat("vertFace", facing.y);
     }
 
     void FixedUpdate()
