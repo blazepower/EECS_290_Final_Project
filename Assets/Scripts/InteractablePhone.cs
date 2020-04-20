18b51698bc946a9c7208f409476bc8936d54e439
Rishik Hombal <hombalrishik@gmail.com>
2020-04-20 18:47:42 -0400

    phone updates

--- Assets/Scripts/InteractablePhone.cs
+++ Assets/Scripts/InteractablePhone.cs
@@ -7,42 +7,33 @@ namespace DefaultNamespace{
     public class InteractablePhone : MonoBehaviour{
         public Collider2D player, phone;
         private Random rand = new Random();
-        private Text[] requests = new Text[60];
+        string[] reqsAsString = new[]{"One plant stat!", "Give me two plants! Right now"};
+        int[] reqsNum = new[]{1, 2};
         private bool gettingRequest;
-        private Text currRequest;
+        public Text currRequest;
         private int numRequests = 0;
-        public Text requestToBeAdded;
         private bool initalTimeOver, ringing;
         private float initialRingTime = 10.0f;
-        private float timeInBetweenRings = 10.0f, ringTime = 3.0f ;
+        private float timeInBetweenRings = 10.0f, ringTime = 3.0f;
         public AudioSource ringSound;
 
         void Start(){
-            string[] reqsAsString = new[]{"One plant stat!", "Give me two plants! Right now"};
-            int[] reqsNum = new[]{1, 2};
-            foreach (string s in reqsAsString){
-                requestToBeAdded.text = s;
-                requests[numRequests] = requestToBeAdded;
-                numRequests++;
-            }
-
+            currRequest = GetComponent<Text>();
             ringSound = GetComponent<AudioSource>();
         }
 
         void getRequest(){
             gettingRequest = true;
-            int index = rand.Next(requests.Length);
-            currRequest = requests[index];
+            int index = rand.Next(reqsAsString.Length);
+            currRequest.text = reqsAsString[index];
             currRequest.gameObject.SetActive(true);
         }
 
         void Update(){
             if (Time.time > initialRingTime){
                 if (ringTime > 0){
-                    Debug.Log(ringTime);
-                    if(!ringSound.isPlaying)
+                    if (!ringSound.isPlaying)
                         ringSound.Play(0);
-                    //Debug.Log("test");
                     ringTime -= (Time.deltaTime);
                     ringing = true;
                 }
@@ -51,14 +42,14 @@ namespace DefaultNamespace{
                     ringing = false;
                     ringSound.Stop();
                     initialRingTime = Time.time + timeInBetweenRings;
+                    gettingRequest = false;
                 }
-                
             }
+
             if (player.IsTouching(phone) && !gettingRequest && ringing){
                 getRequest();
             }
             else{
-                gettingRequest = false;
                 //currRequest.gameObject.SetActive(false);
             }
         }
